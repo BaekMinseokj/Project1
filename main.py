fd3f860e602bddd366169893f5cfa4260ed05771
BaekMinSeok <alstjr3834@naver.com>
2024-01-25 14:39:31 +0900

    두번째 커밋 뺴기 함수 완성 =====

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ def add_func(n1,n2):
     retVal = n1 + n2
     return retVal
 
+def sub_func(n1,n2):
+    return n1 - n2
+    
+
 
 
 ## 전역 변수부
@@ -14,3 +18,6 @@ num1, num2 ,res = 100, 200, 0
 ##메인 코드부
 res = add_func(num1, num2)
 print( num1 ,'+', num2, '=', res)
+
+res = sub_func(num1, num2)
+print( num1 ,'-', num2, '=', res)
